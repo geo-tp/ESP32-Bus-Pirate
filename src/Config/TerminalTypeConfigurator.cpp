e6c0581eddc88b05562acc92a5bedaf0ac414b10
Geo <tp53.geo@gmail.com>
2025-09-22 06:09:23 +0200

    add standalone type to term configurator

--- src/Config/TerminalTypeConfigurator.cpp
+++ src/Config/TerminalTypeConfigurator.cpp
@@ -7,6 +7,9 @@ TerminalTypeEnum TerminalTypeConfigurator::configure() {
     std::vector<std::string> options = {
         TerminalTypeEnumMapper::toString(TerminalTypeEnum::WiFiClient),
         TerminalTypeEnumMapper::toString(TerminalTypeEnum::Serial),
+        #ifdef DEVICE_CARDPUTER
+            TerminalTypeEnumMapper::toString(TerminalTypeEnum::Standalone),
+        #endif
     };
 
     int selected = 1; // Serial
@@ -25,7 +28,7 @@ TerminalTypeEnum TerminalTypeConfigurator::configure() {
     switch (selected) {
         case 0: return TerminalTypeEnum::WiFiClient;
         case 1: return TerminalTypeEnum::Serial;
-        case 2: return TerminalTypeEnum::WiFiAp;
+        case 2: return TerminalTypeEnum::Standalone;
         default: return TerminalTypeEnum::None;
     }
 }
