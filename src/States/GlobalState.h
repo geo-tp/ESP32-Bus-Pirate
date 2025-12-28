199e5e151f169767f58fe80c1d21c2fc95b66e2c
Geo <tp53.geo@gmail.com>
2025-12-28 20:15:24 +0100

    change firmware version number

--- src/States/GlobalState.h
+++ src/States/GlobalState.h
@@ -10,7 +10,7 @@
 class GlobalState {
 private:
     // Version
-    const std::string version = "1.1";
+    const std::string version = "1.2";
 
     //Pin in use
     std::vector<uint8_t> protectedPins;
