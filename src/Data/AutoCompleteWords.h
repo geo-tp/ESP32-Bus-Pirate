9693da9d9693c142b47bc8640c3fd42c25c6d5b8
Geo <tp53.geo@gmail.com>
2026-02-01 00:41:43 +0100

    add cmd words for autocompletion

---
+++ src/Data/AutoCompleteWords.h
@@ -0,0 +1,60 @@
+static const char* const autoCompleteWords[] = {
+
+    // --- General ---
+    "help","mode","man","system","logic","analogic","wizard",
+
+    // --- 1WIRE ---
+    "scan","ping","sniff","read","write","temp","ibutton","eeprom","config",
+
+    // --- UART / HDUART ---
+    "autobaud","bridge","at","spam","glitch","xmodem","swap",
+
+    // --- I2C ---
+    "discover","identify","slave","dump","flood","health","monitor",
+    "recover","jam",
+
+    // --- SPI ---
+    "sdcard","flash",
+
+    // --- 2WIRE / 3WIRE ---
+    "smartcard",
+
+    // --- DIO ---
+    "set","pullup","pulldown","pulse","servo","pwm","toggle","measure","reset",
+
+    // --- LED ---
+    "fill","blink","rainbow","chase","cycle","wave","setprotocol",
+
+    // --- INFRARED ---
+    "send","receive","devicebgone","remote","replay","record","load",
+
+    // --- USB ---
+    "stick","keyboard","mouse","gamepad","jiggle",
+
+    // --- BLUETOOTH ---
+    "pair","spoof","status","server",
+
+    // --- WIFI ---
+    "connect","discovery","probe","deauth","disconnect","ap",
+    "ssh","telnet","nc","nmap","modbus",
+    "http","lookup","webui",
+
+    // --- JTAG ---
+    "swd","jtag",
+
+    // --- I2S ---
+    "play","test",
+
+    // --- CAN ---
+
+    // --- SUBGHZ ---
+    "sweep","decode","bruteforce","trace","listen","setfrequency",
+
+    // --- RFID ---
+    "clone","erase",
+
+    // --- RF24 ---
+    "setchannel",
+
+    nullptr
+};
