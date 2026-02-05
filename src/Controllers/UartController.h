ee72c6f0cd3a234ff682fa02ee4e89ba648dcbec
Geo <tp53.geo@gmail.com>
2026-02-06 00:59:56 +0100

    rework autobaud to detect baud on edges, remove old methods

--- src/Controllers/UartController.h
+++ src/Controllers/UartController.h
@@ -90,21 +90,6 @@ private:
     // Handle UART emulation shell
     void handleEmulation();
 
-    // Scan a baudrate
-    bool scanAtBaudrate(int baud);
-
-    // Send next probe to get a UART response
-    void sendNextProbe(size_t& probeIndex);
-
-    // Update rolling buffer
-    void updateResponse(std::string& response, size_t& asciiCount, size_t maxSize);
-
-    // Check if data response is a valid UART
-    bool isValidResponse(const std::string& response, size_t asciiCount);
-    
-    // Check entropy to determine if a response is a valid UART response
-    float computeEntropy(const std::string& data);
-
     ITerminalView& terminalView;
     IInput& terminalInput;
     IInput& deviceInput;
@@ -138,39 +123,4 @@ private:
         "help\r\n", "?\r\n", "\r\n",
         "\x1B", "\x03", "\x04", "\x1A", "\x11", "\x13"  // ESC, Ctrl+C/Z/D, XON/XOFF
     };
-
-    std::vector<int> baudrates = {
-        // Les plus standards
-        9600,
-        115200,
-        19200,
-        57600,
-        38400,
-
-        // Baudrates spécifiques
-        4800,
-        2400,
-        14400,
-        28800,
-        76800,
-        128000,
-
-        // Haut débit
-        230400,
-        250000,
-        460800,
-
-        // Tres haut débit
-        921600,
-        1000000,
-        1500000,
-        2000000,
-        3000000,
-
-        // legacy
-        1200,
-        600,
-        300
-    };
-
 };
