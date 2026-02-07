49c0fa2b1f249bbb3a6bf0aaed823796ea2899b9
Geo <tp53.geo@gmail.com>
2026-02-07 21:41:27 +0100

    minor info mention fix

--- src/Controllers/UsbS3Controller.cpp
+++ src/Controllers/UsbS3Controller.cpp
@@ -293,8 +293,8 @@ void UsbS3Controller::handleHost() {
     terminalView.println("USB Host: Starting... Plug a USB device to the ESP32. Press [ENTER] to stop.\n");
     terminalView.println("[INFO] USB Host will print connected device info and descriptors.");
     terminalView.println("       This feature works only with USB *devices* (peripherals),");
-    terminalView.println("       such as keyboards, mice, gamepads, USB adapters, etc.\n");
-
+    terminalView.println("       such as keyboards, mice, gamepads, USB adapters, etc.");
+    terminalView.println("       Make sure your board provides 5V on the USB port.\n");
     while (true) {
         char c = terminalInput.readChar();
         if (c == '\r' || c == '\n') break;
--- src/Transformers/TerminalCommandTransformer.cpp
+++ src/Transformers/TerminalCommandTransformer.cpp
@@ -124,28 +124,6 @@ void TerminalCommandTransformer::autoCorrectSubCommand(TerminalCommand& cmd) con
     // lowercase subcmd
     for (char& c : sub) c = (char)std::tolower((unsigned char)c);
 
-    // aliases for protocols
-    struct Alias { const char* from; const char* to; };
-    static const Alias aliases[] = {
-        {"onewire",   "1wire"},
-        {"1w",        "1wire"},
-        {"twowire",   "2wire"},
-        {"2w",        "2wire"},
-        {"threewire", "3wire"},
-        {"3w",        "3wire"},
-        {"i2",        "i2c"},
-        {"serial",    "uart"},
-        {"eth",       "ethernet"},
-        {"bt",        "bluetooth"},
-        {"ble",       "bluetooth"},
-        {"ir",        "infrared"},
-        {"sub",       "subghz"},
-        {"rf",        "rf24"},
-        {"nfc",       "rfid"},
-
-        {nullptr, nullptr}
-    };
-
     for (int i = 0; aliases[i].from; ++i) {
         if (sub == aliases[i].from) {
             cmd.setSubcommand(aliases[i].to);
