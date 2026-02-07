46d196a5816350ef94e2c22f821ecff8df11af2c
Geo <tp53.geo@gmail.com>
2026-02-07 07:34:00 +0100

    add modes to autocompletion words

--- src/Data/AutoCompleteWords.h
+++ src/Data/AutoCompleteWords.h
@@ -29,7 +29,7 @@ static const char* const autoCompleteWords[] = {
     "send","receive","devicebgone","remote","replay","record","load",
 
     // --- USB ---
-    "stick","keyboard","mouse","gamepad","jiggle",
+    "stick","keyboard","mouse","gamepad","jiggle", "host",
 
     // --- BLUETOOTH ---
     "pair","spoof","status","server",
@@ -45,8 +45,6 @@ static const char* const autoCompleteWords[] = {
     // --- I2S ---
     "play","test",
 
-    // --- CAN ---
-
     // --- SUBGHZ ---
     "sweep","decode","bruteforce","trace","listen","setfrequency",
 
@@ -55,6 +53,17 @@ static const char* const autoCompleteWords[] = {
 
     // --- RF24 ---
     "setchannel",
+
+    // --- MODE shortcuts ---
+    "m hiz","m 1wire","m uart","m hduart","m i2c","m spi",
+    "m 2wire","m 3wire","m dio","m led","m infrared",
+    "m usb","m bluetooth","m wifi","m jtag","m i2s","m can",
+    "m ethernet","m subghz","m rfid","m rf24",
+
+    "mode hiz","mode 1wire","mode uart","mode hduart","mode i2c","mode spi",
+    "mode 2wire","mode 3wire","mode dio","mode led","mode infrared",
+    "mode usb","mode bluetooth","mode wifi","mode jtag","mode i2s","mode can",
+    "mode ethernet","mode subghz","mode rfid","mode rf24",
 
     nullptr
 };
