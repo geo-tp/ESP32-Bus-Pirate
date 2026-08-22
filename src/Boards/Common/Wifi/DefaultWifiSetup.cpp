a4b24a6b8bbdcd0877b493a48aab2439cfe4e689
Roberto Spadim <rspadim@gmail.com>
2026-08-22 16:58:40 -0300

    Include C6DevKit board in default WiFi setup

--- src/Boards/Common/Wifi/DefaultWifiSetup.cpp
+++ src/Boards/Common/Wifi/DefaultWifiSetup.cpp
@@ -1,4 +1,4 @@
-#if defined(DEVICE_M5STAMPS3) || defined(DEVICE_S3DEVKIT) || defined(DEVICE_VISION_MASTER_T190) || defined(DEVICE_CUSTOM)
+#if defined(DEVICE_M5STAMPS3) || defined(DEVICE_S3DEVKIT) || defined(DEVICE_VISION_MASTER_T190) || defined(DEVICE_CUSTOM) || defined(DEVICE_C6DEVKIT)
 
 #include "Boards/Common/Wifi/DefaultWifiSetup.h"
 #include <Preferences.h>
--- src/Boards/Common/Wifi/DefaultWifiSetup.h
+++ src/Boards/Common/Wifi/DefaultWifiSetup.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#if defined(DEVICE_M5STAMPS3) || defined(DEVICE_S3DEVKIT) || defined(DEVICE_VISION_MASTER_T190) || defined(DEVICE_CUSTOM)
+#if defined(DEVICE_M5STAMPS3) || defined(DEVICE_S3DEVKIT) || defined(DEVICE_VISION_MASTER_T190) || defined(DEVICE_CUSTOM) || defined(DEVICE_C6DEVKIT)
 
 bool setupDefaultWifi();
 
